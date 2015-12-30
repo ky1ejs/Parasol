7c9de81cb26f0255be99fc587750af0ed9df0b06
Kyle McAlpine <kyle@loot.io>
2015-12-30 22:38:27 +0000

    Initial commit

---
+++ Parasol/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Parasol
+//
+//  Created by Kyle McAlpine on 30/12/2015.
+//  Copyright © 2015 Loot Financial Services Ltd. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
