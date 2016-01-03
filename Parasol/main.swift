e4111b371cbd39b61c478f07db42255f88d09aa7
Kyle McAlpine <kyle@loot.io>
2016-01-03 15:51:55 +0000

    Printing coverage data

--- Parasol/main.swift
+++ Parasol/main.swift
@@ -9,8 +9,14 @@
 import Foundation
 
 // TODO: make passing Xcode project as an optional argument
-if let xcodeProject = XcodeProject.findXcodeProjectInCurrentDirectory() {
-    
+if let xcodeProject = XcodeProject.findXcodeProjectInCurrentDirectory(), coverageProfdataPath = xcodeProject.coverageProfdataPath, codeCoverageExecutablePath = xcodeProject.codeCoverageExecutablePath {
+    print(coverageProfdataPath)
+    print(codeCoverageExecutablePath)
+    let task = NSTask()
+    task.launchPath = "/usr/bin/xcrun"
+    task.arguments = ["llvm-cov", "show", "-instr-profile", coverageProfdataPath, codeCoverageExecutablePath]
+    task.launch()
+    task.waitUntilExit()
 } else {
     print("Couldn't find an Xcode project")
 }
