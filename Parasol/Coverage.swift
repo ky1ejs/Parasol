95f8ad655843fb7b58a935db4a37ba28ab22e6bf
Kyle McAlpine <kyle@loot.io>
2016-01-10 15:08:16 +0000

    Printing llvm-cov report

--- Parasol/Coverage.swift
+++ Parasol/Coverage.swift
@@ -9,11 +9,29 @@
 import Foundation
 
 struct Coverage {
+    let lines: [String]
+    
     static func showCoverage(profdataPath: String, executablePath: String) {
         let task = NSTask()
         task.launchPath = "/usr/bin/xcrun"
         task.arguments = ["llvm-cov", "show", "-instr-profile", profdataPath, executablePath]
         task.launch()
         task.waitUntilExit()
+    }
+    
+    static func showCoverageReport(profdataPath: String, executablePath: String) -> Coverage {
+        let task = NSTask()
+        task.launchPath = "/usr/bin/xcrun"
+        task.arguments = ["llvm-cov", "report", "-filename-equivalence", "-instr-profile", profdataPath, executablePath]
+        let outPipe = NSPipe()
+        task.standardOutput = outPipe
+        task.launch()
+        task.waitUntilExit()
+        let report = String(data: outPipe.fileHandleForReading.readDataToEndOfFile(), encoding: NSUTF8StringEncoding)!
+        var lines = [String]()
+        report.enumerateLines { (line, stop) -> () in
+            lines.append(line)
+        }
+        return Coverage(lines: lines)
     }
 }
