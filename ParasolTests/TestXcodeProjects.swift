44b658ff213b0b9755007af988f283369a277ec9
Kyle McAlpine <kyle@loot.io>
2016-01-31 22:49:05 +0000

    Fixed error. fileManager moves itself to parent after deleting current dir

--- ParasolTests/TestXcodeProjects.swift
+++ ParasolTests/TestXcodeProjects.swift
@@ -14,21 +14,17 @@ class TestXcodeProjects {
     static let secondProjectPath = NSURL(string: "A-Project/A.xcodeproj")!
     
     class func unzipTestProjects() {
-        let fileManager = NSFileManager.defaultManager()
-        
         // Unzip test Xcode project (Parasol.xcodeproj)
         let zipPath = NSBundle(forClass: self).pathForResource("TestXcodeProjects", ofType: "zip")
-        try! SSZipArchive.unzipFileAtPath(zipPath, toDestination: fileManager.currentDirectoryPath, overwrite: false, password: nil)
+        try! SSZipArchive.unzipFileAtPath(zipPath, toDestination: XcodeProject.fileManager.currentDirectoryPath, overwrite: false, password: nil)
         
         // Change working dir to see test Xcode project
         XcodeProject.fileManager.changeCurrentDirectoryPath("TestXcodeProjects")
     }
     
     class func cleanUp() {
-        let fileManager = NSFileManager.defaultManager()
-        let currentDirURL = NSURL(string: fileManager.currentDirectoryPath)!
-        fileManager.changeCurrentDirectoryPath(currentDirURL.URLByDeletingLastPathComponent!.path!)
-        try! fileManager.removeItemAtPath(currentDirURL.path!)
-        XcodeProject.fileManager.changeCurrentDirectoryPath("../")
+        let currentDirURL = NSURL(string: XcodeProject.fileManager.currentDirectoryPath)!
+        XcodeProject.fileManager.changeCurrentDirectoryPath(currentDirURL.URLByDeletingLastPathComponent!.path!)
+        try! XcodeProject.fileManager.removeItemAtPath(currentDirURL.path!) // fileManager will automatically move to parent
     }
 }
