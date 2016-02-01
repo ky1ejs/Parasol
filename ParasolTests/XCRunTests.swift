7aa78e817543fa01065dbf1c06dc35add29e8144
Kyle McAlpine <kyle@loot.io>
2016-02-01 09:00:57 +0000

    XCRun tests added

---
+++ ParasolTests/XCRunTests.swift
@@ -0,0 +1,31 @@
+//
+//  XCRunTests.swift
+//  Parasol
+//
+//  Created by Kyle McAlpine on 01/02/2016.
+//  Copyright © 2016 Loot Financial Services Ltd. All rights reserved.
+//
+
+import XCTest
+
+class XCRunTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+        TestXcodeProjects.unzipTestProjects()
+    }
+    
+    override func tearDown() {
+        TestXcodeProjects.cleanUp()
+        super.tearDown()
+    }
+    
+    func testCoverageWithFormat() {
+        let project = XcodeProject.findXcodeProjectInCurrentDirectory()
+        XCTAssertNotNil(project?.targets.first)
+        let target = project?.targets.first!
+        target?.runTests()
+        let coverage = try? XCRun.coverageWithFormat(.Analysis, profdataPath: target!.codeCoverageProfdataPath!, executablePath: target!.executablePath!)
+        XCTAssertNotNil(coverage)
+    }
+}
